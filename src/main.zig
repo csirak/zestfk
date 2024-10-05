c602859db914c7e71cf6959e60102363f163da6e
csirak1528 <calebsirak@gmail.com>
2024-10-05 18:48:08 -0400

    mmap test

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ pub const instructions = @import("instructions.zig");
 pub fn main() !void {
     //    const instr = instructions.setZero(4);
 
-    const instrs = [_]u32{instructions.ret(30)};
+    const instrs = [_]u32{instructions.ret};
     try instructions.execute(&instrs);
 }
 
