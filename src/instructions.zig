9c4ab18066472ae6f6717c6c9b3ac2a7581b1485
csirak1528 <calebsirak@gmail.com>
2024-10-05 16:55:51 -0400

    mmap test

---
+++ src/instructions.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+
+pub fn addi(rd: u5, rn: u5, imm: u12) u32 {
+    const op: u32 = 0b00_10001; // Opcode for ADD (immediate)
+    const sf: u32 = 1; // 64-bit instruction
+    const sh: u32 = 0; // No shift
+
+    return op << 24 |
+        sf << 31 |
+        sh << 22 |
+        @as(u32, imm) << 10 |
+        @as(u32, rn) << 5 |
+        @as(u32, rd);
+}
+
+pub fn subi(rd: u5, rn: u5, imm: u12) u32 {
+    const op: u32 = 0b10_10001; // Opcode for SUB (immediate)
+    const sf: u32 = 1; // 64-bit instruction
+    const sh: u32 = 0; // No shift
+    return op << 24 |
+        sf << 31 |
+        sh << 22 |
+        @as(u32, imm) << 10 |
+        @as(u32, rn) << 5 |
+        @as(u32, rd);
+}
+
+pub fn clearRegister(rd: u5) u32 {
+    const op: u32 = 0b100_10100_00000000000000; // Opcode for ORR (immediate)
+    const sf: u32 = 1; // 1 for 64-bit, 0 for 32-bit
+    const opc: u32 = 1; // For MOV alias of ORR
+    return op |
+        sf << 31 |
+        opc << 29 |
+        @as(u32, rd);
+}
+
+pub fn executeInstruction(instr: []const u32) void {
+    const prot = std.posix.PROT;
+    _ = try std.posix.mmap(
+        null,
+        @intCast(instr.len),
+        prot.READ | prot.EXEC,
+        .PRIVATE,
+        -1,
+        0,
+    );
+}
+
+test "clear reg" {
+    const instr = clearRegister(4);
+
+    const instructions = [_]u32{instr};
+    executeInstruction(&instructions);
+
+    var x: u64 = 0;
+    asm volatile ("mov %[x], x4"
+        : [x] "=r" (x),
+    );
+    std.testing.expectEqual(x, 0);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 pub const parser = @import("parser.zig");
+pub const instructions = @import("instructions.zig");
 
 pub fn main() !void {}
 
