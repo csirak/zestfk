f70aacfd26ff0d8e188619a47f3db1946f20502e
csirak1528 <calebsirak@gmail.com>
2024-10-05 17:24:05 -0400

    mmap test

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,13 @@ const std = @import("std");
 pub const parser = @import("parser.zig");
 pub const instructions = @import("instructions.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+        const instr = instructions.clearRegister(4);
+
+    const instructions = [_]u32{instr};
+    try instructions.executeInstruction(&instructions);
+
+}
 
 test {
     std.testing.refAllDecls(@This());
