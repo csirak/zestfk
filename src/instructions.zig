983bdfffbdf242a376cf2b2424a781743edf7a63
csirak1528 <calebsirak@gmail.com>
2024-10-05 20:03:50 -0400

    mmap test

--- src/instructions.zig
+++ src/instructions.zig
@@ -23,6 +23,13 @@ pub fn subi(rd: u5, imm: u12) u32 {
         @as(u32, rd);
 }
 
+pub fn cbnz(rd: u5, label: i19) u32 {
+    const opcode: u32 = 0xb5000000;
+    return opcode |
+        @as(u32, label) << 5 |
+        @as(u32, rd);
+}
+
 pub fn execute(instr: []const u32) !void {
     const prot = std.posix.PROT;
     const exec_ptr = try std.posix.mmap(
@@ -79,6 +86,17 @@ test "add reg" {
 
 test "sub reg" {
     const instructions = [_]u32{ setZero(4), addi(4, 489), subi(4, 69), ret };
+    try execute(&instructions);
+
+    var x: u64 = 0;
+    asm volatile ("mov %[x], x4"
+        : [x] "=r" (x),
+    );
+    try std.testing.expectEqual(420, x);
+}
+
+test "cbnz" {
+    const instructions = [_]u32{ setZero(4), addi(4, 489), cbnz(4, 1), subi(4, 69), ret };
     try execute(&instructions);
 
     var x: u64 = 0;
