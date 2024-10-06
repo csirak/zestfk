8c9232318ea38726f38899ef017c7a15c36d9c0e
csirak1528 <calebsirak@gmail.com>
2024-10-06 00:23:31 -0400

    mmap test

--- src/codegen.zig
+++ src/codegen.zig
@@ -10,16 +10,17 @@ pub fn codegen(instrs: std.ArrayList(parser.Instruction), allocator: std.mem.All
         aarch64_instrs[2 * i] = switch (instr.command) {
             .right => instructions.addi(azm.DATA_PTR, @truncate(instr.value)),
             .left => instructions.subi(azm.DATA_PTR, @truncate(instr.value)),
-            .read => instructions.blr(azm.READ_HANDLER),
-            .write => instructions.blr(azm.WRITE_HANDLER),
             .jumpIfNotZero => instructions.cbnz(azm.ACCUM, @intCast(instr.value * 2)),
             .jumpBackIf => instructions.cbz(azm.ACCUM, @intCast(instr.value * 2)),
             .inc => instructions.addi(azm.ACCUM, @truncate(instr.value)),
             .dec => instructions.subi(azm.ACCUM, @truncate(instr.value)),
+            else => instructions.nop,
         };
 
         aarch64_instrs[2 * i + 1] = switch (instr.command) {
             .left, .right => instructions.ldda,
+            .read => instructions.blr(azm.READ_HANDLER),
+            .write => instructions.blr(azm.WRITE_HANDLER),
             else => instructions.nop,
         };
     }
--- src/handlers.zig
+++ src/handlers.zig
@@ -8,11 +8,15 @@ pub inline fn execPrologue() void {
     azm.writeReadHandler(@intFromPtr(&readHandler));
 }
 
-inline fn epilogue(data_ptr: u64, accum: u64) void {
+inline fn epilogue(data_ptr: u64, accum: u64, ra: u64) void {
     azm.writeDataPtr(data_ptr);
     azm.writeAccum(accum);
     azm.writeWriteHandler(@intFromPtr(&writeHandler));
     azm.writeReadHandler(@intFromPtr(&readHandler));
+    asm volatile ("mov x7, %[ra]"
+        :
+        : [ra] "r" (ra),
+    );
 }
 
 pub fn readHandler() callconv(.C) void {
@@ -22,15 +26,15 @@ pub fn readHandler() callconv(.C) void {
     const stdin = std.io.getStdIn().reader();
     std.debug.print("ENTER BYTE: ", .{});
     _ = stdin.read(write_to[0..1]) catch @panic("READ FAILED");
-    epilogue(data_ptr, accum);
+    const ra = @returnAddress();
+    epilogue(data_ptr, accum, ra);
 }
 
 pub fn writeHandler() callconv(.C) void {
     const ra = @returnAddress();
-
     const data_ptr = azm.getDataPtr();
     const accum = azm.getAccum();
     const read_from: *u8 = @ptrFromInt(data_ptr);
     std.debug.print("HEY: {c}\n ra: {x}", .{ read_from.*, ra });
-    epilogue(data_ptr, accum);
+    epilogue(data_ptr, accum, ra);
 }
