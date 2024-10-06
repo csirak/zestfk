2e86804302cf7f79fdd724e64edc0b0e201ee8cc
csirak1528 <calebsirak@gmail.com>
2024-10-06 00:57:00 -0400

    mmap test

--- src/parser.zig
+++ src/parser.zig
@@ -48,7 +48,7 @@ pub fn parse(code: []const u8, allocator: std.mem.Allocator) !std.ArrayList(Inst
     var cur_command: usize = 0;
     while (cur_command < code.len) {
         switch (code[cur_command]) {
-            ' ', '\n', '\t', '\r' => {
+            ' ', '\n', 10, '\t', '\r' => {
                 cur_command += 1;
                 continue;
             },
