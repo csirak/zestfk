47fee237cc2473c36adbe471b68b6eb5ad576dcb
csirak1528 <calebsirak@gmail.com>
2024-10-05 22:56:20 -0400

    mmap test

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 pub const parser = @import("parser.zig");
 pub const instructions = @import("instructions.zig");
 pub const runtime = @import("runtime.zig");
-pub const azm = @import("azm.zig");
+pub const azm = @import("asm.zig");
 
 pub fn main() !void {
     const instrs = [_]u32{
