ec5e5a3dffb55f613d209cc00d4dd3e22985993d
csirak1528 <calebsirak@gmail.com>
2024-10-05 22:55:53 -0400

    mmap test

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,17 @@ const std = @import("std");
 pub const parser = @import("parser.zig");
 pub const instructions = @import("instructions.zig");
 pub const runtime = @import("runtime.zig");
+pub const azm = @import("azm.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const instrs = [_]u32{
+        instructions.setZero(0),
+        instructions.addi(0, 1),
+        instructions.blr(azm.READ_HANDLER),
+        instructions.ret,
+    };
+    try runtime.execute(&instrs, 30000);
+}
 
 test {
     std.testing.refAllDecls(@This());
