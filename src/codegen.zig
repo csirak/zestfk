b3f08f5bb46ded1f1fa0a163ae2946a0a496f4c5
csirak1528 <calebsirak@gmail.com>
2024-10-05 23:32:57 -0400

    mmap test

--- src/codegen.zig
+++ src/codegen.zig
@@ -4,23 +4,25 @@ const parser = @import("parser.zig");
 const instructions = @import("instructions.zig");
 const azm = @import("asm.zig");
 
-pub fn codegen(instrs: std.Arraylist(parser.Instruction), allocator: std.mem.Allocator) []u32 {
-    var aarch64_instrs = allocator.alloc(u32, instrs.items.len * 2);
+pub fn codegen(instrs: std.ArrayList(parser.Instruction), allocator: std.mem.Allocator) ![]u32 {
+    var aarch64_instrs = try allocator.alloc(u32, instrs.items.len * 2);
     for (instrs.items, 0..) |instr, i| {
         aarch64_instrs[2 * i] = switch (instr.command) {
-            .right => instructions.addi(azm.data_ptr, @truncate(instr.value)),
-            .left => instructions.subi(azm.data_ptr, @truncate(instr.value)),
-            .read => instructions.blr(azm.read_handler),
-            .write => instructions.blr(azm.write_handler),
+            .right => instructions.addi(azm.DATA_PTR, @truncate(instr.value)),
+            .left => instructions.subi(azm.DATA_PTR, @truncate(instr.value)),
+            .read => instructions.blr(azm.READ_HANDLER),
+            .write => instructions.blr(azm.WRITE_HANDLER),
             else => instructions.ldda,
         };
 
         aarch64_instrs[2 * i + 1] = switch (instr.command) {
-            .jumpIfNotZero => instructions.cbnz(azm.ACCUM, instr.value * 2),
-            .jumpBackIf => instructions.cbz(azm.ACCUM, instr.value * 2),
+            .jumpIfNotZero => instructions.cbnz(azm.ACCUM, @intCast(instr.value * 2)),
+            .jumpBackIf => instructions.cbz(azm.ACCUM, @intCast(instr.value * 2)),
             .inc => instructions.addi(azm.ACCUM, @truncate(instr.value)),
             .dec => instructions.subi(azm.ACCUM, @truncate(instr.value)),
             else => instructions.nop,
         };
     }
+
+    return aarch64_instrs;
 }
--- src/main.zig
+++ src/main.zig
@@ -2,16 +2,15 @@ const std = @import("std");
 pub const parser = @import("parser.zig");
 pub const instructions = @import("instructions.zig");
 pub const runtime = @import("runtime.zig");
+pub const codegen = @import("codegen.zig");
 pub const azm = @import("asm.zig");
 
 pub fn main() !void {
-    const instrs = [_]u32{
-        instructions.setZero(0),
-        instructions.addi(0, 1),
-        instructions.blr(azm.READ_HANDLER),
-        instructions.ret,
-    };
-    try runtime.execute(&instrs, 30000, std.heap.page_allocator);
+    const code = "+++>+++";
+    const alloc = std.heap.page_allocator;
+    const parsed = try parser.parse(code, alloc);
+    const instrs = try codegen.codegen(parsed, alloc);
+    try runtime.execute(instrs, 30000, alloc);
 }
 
 test {
