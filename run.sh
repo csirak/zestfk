c0f0cc93da87c36022dd817971d1e2d64f9ece44
csirak1528 <calebsirak@gmail.com>
2024-10-07 15:32:08 -0400

    mmap test

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ DEFAULT_ZIG_EXEC="/home/debian/zig-linux-aarch64-0.13.0/zig"
 ZIG_EXEC="${1:-$DEFAULT_ZIG_EXEC}"
 
 # SSH command
-ssh debian@pi1 "cd zestfk && git pull && $ZIG_EXEC build run"
+ssh debian@pi1 "cd zestfk && git pull && $ZIG_EXEC build run --release=fast"
