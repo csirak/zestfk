cec91593ed8c5914b27b25b749311518c9e382d2
csirak1528 <calebsirak@gmail.com>
2024-10-07 15:12:30 -0400

    mmap test

--- src/codegen.zig
+++ src/codegen.zig
@@ -13,7 +13,7 @@ pub fn codegen(instrs: std.ArrayList(parser.Instruction), allocator: std.mem.All
             .right => instructions.addi(azm.DATA_PTR, @intCast(instr.value * 8)),
             .left => instructions.subi(azm.DATA_PTR, @intCast(instr.value * 8)),
             .jumpIfNotZero => instructions.cbz(azm.ACCUM, @intCast(instr.value * 2)),
-            .jumpBackIf => instructions.cbnz(azm.ACCUM, @intCast(@as(i16, @intCast( instr.value ) ) * -2)),
+            .jumpBackIf => instructions.cbnz(azm.ACCUM, @intCast(@as(i16, @intCast(instr.value)) * -2)),
             .inc => instructions.addi(azm.ACCUM, @truncate(instr.value)),
             .dec => instructions.subi(azm.ACCUM, @truncate(instr.value)),
             .read => instructions.blr(azm.READ_HANDLER),
@@ -22,17 +22,17 @@ pub fn codegen(instrs: std.ArrayList(parser.Instruction), allocator: std.mem.All
 
         aarch64_instrs[2 * i + 1] = switch (instr.command) {
             .left, .right => instructions.ldda,
-            .inc, .dec=> instructions.stda ,
+            .inc, .dec => instructions.stda,
             else => instructions.nop,
         };
     }
 
     const end = instrs.items.len * 2;
-    const ret_label_32: u32= @truncate( @intFromPtr(&ret_label) );
-    const ret_label_upper : u16= @truncate( ret_label_32 >> 16);
-    const ret_label_lower : u16= @truncate( ret_label_32 & 0xFFFF);
-    aarch64_instrs[end ] = instructions.movz(azm.LINK_REGISTER,ret_label_lower);
-    aarch64_instrs[end + 1] = instructions.movz_shift16(29,ret_label_upper);
+    const ret_label_32: u32 = @truncate(@intFromPtr(&ret_label));
+    const ret_label_upper: u16 = @truncate(ret_label_32 >> 16);
+    const ret_label_lower: u16 = @truncate(ret_label_32 & 0xFFFF);
+    aarch64_instrs[end] = instructions.movz(azm.LINK_REGISTER, ret_label_lower);
+    aarch64_instrs[end + 1] = instructions.movz_shift16(29, ret_label_upper);
     aarch64_instrs[end + 2] = instructions.addret29;
     aarch64_instrs[end + 3] = instructions.ret;
     return aarch64_instrs;
--- src/handlers.zig
+++ src/handlers.zig
@@ -4,12 +4,16 @@ const runtime = @import("runtime.zig");
 
 pub const instructions = @import("instructions.zig");
 
+const BUF_SIZE = 1024;
+var write_buffer = [_]u8{0} ** BUF_SIZE;
+var cur: usize = 0;
+
 pub inline fn execPrologue() void {
     azm.writeWriteHandler(@intFromPtr(&writeHandler));
     azm.writeReadHandler(@intFromPtr(&readHandler));
 }
 
-inline fn epilogue(data_ptr: u64, accum: u64 ) void {
+inline fn epilogue(data_ptr: u64, accum: u64) void {
     azm.writeDataPtr(data_ptr);
     azm.writeAccum(accum);
     azm.writeWriteHandler(@intFromPtr(&writeHandler));
@@ -20,8 +24,9 @@ pub fn readHandler() callconv(.C) void {
     const data_ptr = azm.getDataPtr();
     const accum = azm.getAccum();
     const write_to: [*]u8 = @ptrFromInt(data_ptr);
-    const stdin = std.io.getStdIn().reader();
+    writeBufferFlush();
     std.debug.print("ENTER BYTE: ", .{});
+    const stdin = std.io.getStdIn().reader();
     _ = stdin.read(write_to[0..1]) catch @panic("READ FAILED");
     epilogue(data_ptr, accum);
 }
@@ -31,6 +36,18 @@ pub fn writeHandler() callconv(.C) void {
     const accum = azm.getAccum();
     const read_from: *u64 = @ptrFromInt(data_ptr);
     const byte: u8 = @truncate(read_from.*);
-    std.debug.print("{c}", .{byte});
+    write_buffer[cur] = byte;
+
+    if (cur + 1 >= BUF_SIZE) {
+        writeBufferFlush();
+    } else {
+        cur += 1;
+    }
     epilogue(data_ptr, accum);
+}
+
+pub fn writeBufferFlush() void {
+    const stdout = std.io.getStdOut().writer();
+    _ = stdout.write(write_buffer[0..cur]) catch @panic("WRITE FAILED");
+    cur = 0;
 }
